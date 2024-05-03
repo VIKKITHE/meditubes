dbc17c2b8a80a080ff0a50f97bf63f28ef18d610
VIKKITHE <162552115+VIKKITHE@users.noreply.github.com>
2024-05-03 15:47:39 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ 
+<?php include_once("inc/head_script.php"); ?>
+  <!-- =======================================================
+  * Template Name: Medicio
+  * Template URL: https://bootstrapmade.com/medicio-free-bootstrap-theme/
+  * Updated: Mar 17 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Top Bar ======= -->
+  <div id="topbar" class="d-flex align-items-center fixed-top">
+    <div class="container d-flex align-items-center justify-content-center justify-content-md-between">
+      <div class="align-items-center d-none d-md-flex">
+        <i class="bi bi-clock"></i> Monday - Saturday, 9AM to 7PM
+      </div>
+      <div class="d-flex align-items-center">
+        <i class="bi bi-phone"></i> Call us now +1 5589 55488 55
+      </div>
+    </div>
+  </div>
+
+  <!-- ======= Header ======= -->
+  <?php include_once("inc/header.php"); ?><!-- End Header -->
+
+  
+
+  <main id="main">
+
+     <!-- ======= About Us Section ======= -->
+     <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>About Us</h2>
+          <p>Medical aid comprises a diverse range of devices, instruments, machines, and tools used in healthcare settings for diagnosis, treatment, monitoring, and rehabilitation of patients. These equipment are essential in providing accurate diagnoses, effective treatments, and improved patient outcomes.
+          </p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6" data-aos="fade-right">
+            <img src="assets/img/about.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <h1>Importance of Medical aid
+            </h1>
+            <ul>
+              <li><i class="bi bi-check-circle"></i> Enhanced Patient Care: Medical aid enables precise diagnosis, effective treatment, and improved patient outcomes.
+              </li>
+              <li><i class="bi bi-check-circle"></i> Efficiency and Accuracy: These devices streamline healthcare processes and ensure accurate medical interventions.
+              </li>
+              <li><i class="bi bi-check-circle"></i> Safety and Reliability: Rigorous testing and quality control ensure the safety and reliability of medical equipment.
+              </li>
+            </ul>
+            <p>
+              Medical aid plays a critical role in modern healthcare, enabling healthcare professionals to deliver high-quality care and improve patient outcomes. Advances in technology continue to drive innovation in medical devices, shaping the future of healthcare delivery worldwide.
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Us Section -->
+
+   
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+    <?php include_once("inc/fotter.php"); ?><!-- End Footer -->
+    <?php include_once("inc/foot_script.php"); ?>
+  
+
+</body>
+
+</html>
